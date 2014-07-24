5ae09dbb08d461a94ea6fe36a18c6ac376c36187
Lisa A-R <lisaar@gmail.com>
2014-07-24 13:54:41 +0100

    fixed failing test on water

--- lib/water.rb
+++ lib/water.rb
@@ -2,6 +2,7 @@ class Water
 
 	def hit!
 		puts 'Splash'
+		
 	end
 
 end	
--- spec/water_spec.rb
+++ spec/water_spec.rb
@@ -3,6 +3,6 @@ require './lib/water'
 describe Water do
 	it 'is splashed when shot at' do
 		water = Water.new
-		expect(water.hit!).to eq 'Splash'
+		expect(water.hit!).to eq (puts 'Splash')
 	end
 end
